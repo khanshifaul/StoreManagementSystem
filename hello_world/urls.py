dc6298eb7ea803b4b117cec04d2b97eb5acd523e
khanshifaul <khanshifaul@gmail.com>
2021-07-04 11:22:34 +0600

    Initial Commit

--- hello_world/urls.py
+++ hello_world/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='index'),
+    path('features/', views.features, name='features'),
+    path('pricing/', views.pricing, name='pricing'),
+    path('faqs/', views.faqs, name='faqs'),
+    path('about/', views.about, name='about'),
+    path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
 ]
--- hello_world/views.py
+++ hello_world/views.py
@@ -1,5 +1,35 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'hw_index.html')
+
+
+def features(request):
+    return render(request, 'hw_features.html')
+
+
+def pricing(request):
+    return render(request, 'hw_pricing.html')
+
+
+def faqs(request):
+    return render(request, 'hw_faqs.html')
+
+
+def about(request):
+    return render(request, 'hw_about.html')
+
+
+def register(request):
+    return render(request, 'hw_register.html')
+
+
+def login(request):
+    return render(request, 'hw_login.html')
+
+
+def logout(request):
+    return render(request, 'logout.html')
